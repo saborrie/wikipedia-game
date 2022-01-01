27d8f82057a0d8f06fe9440de46b0915a1f77830
Steven Borrie <saborrie@gmail.com>
2022-01-01 22:03:18 +0000

    added serilog and signalR compression

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wikipedia Game
+
+Inspired by Tom Scott's "Two of these people are lying", this work is an attmept to make it playable online, and also as a convenient way to organise the game and obfuscate who has chosen which word when the alternative is to write them by hand.
+
+## Feature list
+
+- creating
+- joining
+- choosing a word
+- starting the game
+- all screens updating together
+- joining as a spectator when a round is in progress
+- keeping score
--- WikipediaGame.Server/Program.cs
+++ WikipediaGame.Server/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+using Serilog;
+
 using WikipediaGame.Server.Hubs;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+// makes signalR use compression.
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 builder.Services.AddSignalR();
 
